ddd44e672f32b6077e9393f73fbd40a75808f386
tanishkgupta-18 <144482075+tanishkgupta-18@users.noreply.github.com>
2025-06-19 12:51:01 +0530

    push

--- leet2938.cpp
+++ leet2938.cpp
@@ -1,3 +1,15 @@
+/*
+There are n balls on a table, each ball has a color black or white.
+
+You are given a 0-indexed binary string s of length n, where 1 and 0 represent black and white balls, respectively.
+
+In each step, you can choose two adjacent balls and swap them.
+
+Return the minimum number of steps to group all the black balls to the right and all the white balls to the left.
+
+
+*/
+
 long long minimumSteps(string s) {
     int n = s.length(), ones = 0;
     long long ans = 0;
